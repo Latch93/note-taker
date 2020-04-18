dd961eeedaff9cb118e821b96b3166e00e07620a
Bren519 <bgduhon2gmail.com>
2020-04-18 12:38:39 -0500

    connected query to note cards

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "password";
-$dbname = "note-taker";
+$dbname = "NoteKnight";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
